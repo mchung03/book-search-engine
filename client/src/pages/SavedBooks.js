a2685cdee29dd6b32fd8af5f4254c351a22e0142
mchung03 <minhee.chung14@gmail.com>
2023-09-05 21:15:09 -0700

    refactored savedbooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -15,10 +15,39 @@ import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
+  const [ deleteBook ] = useMutation(REMOVE_BOOK);
+
   const { loading, data } = useQuery( GET_ME );
   const userData = data?.me
+  // const [userData, setUserData] = useState({});
 
-  const [ deleteBook ] = useMutation(REMOVE_BOOK);
+  // // use this to determine if `useEffect()` hook needs to run again
+  // const userDataLength = Object.keys(userData).length;
+
+  // useEffect(() => {
+  //   const getUserData = async () => {
+  //     try {
+  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+  //       if (!token) {
+  //         return false;
+  //       }
+
+  //       const response = await getMe(token);
+
+  //       if (!response.ok) {
+  //         throw new Error('something went wrong!');
+  //       }
+
+  //       const user = await response.json();
+  //       setUserData(user);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+
+  //   getUserData();
+  // }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -31,18 +60,12 @@ const SavedBooks = () => {
     try {
       const response = await deleteBook({
         variables: {
-          "bookData": {
-            "authors": userData.savedBooks.authors,
-            "bookId": userData.savedBooks.bookId,
-            "description": userData.savedBooks.description,
-            "image": userData.savedBooks.image,
-            "title": userData.savedBooks.title
-          }
+          "bookId": bookId
         }
       });
 
-      const updatedUser = await response.data.removeBook;
-      setUserData(updatedUser);
+      // const updatedUser = await response.json();
+      // setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
